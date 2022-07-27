55f9bc3a78518919544d3ca5a8cd958ad4c127f2
SakshamKhanuja <ssk@outlook.ie>
2022-07-27 11:24:08 +0530

    Add precision difference

--- src/Main.java
+++ src/Main.java
@@ -28,5 +28,19 @@ public class Main {
         // Initializing a variable of type "double" - Default type for real numbers.
         double myDoubleValue = 5.25D;       // double myDoubleValue = 5.25; -> VALID
         System.out.println("Double value = " + myDoubleValue);
+
+        // Dividing 5 by 3 in case of "int" - Doesn't handle remainder of the division.
+        int myIntDivide = 5 / 3;            // 1
+        System.out.println("Int divide = " + myIntDivide);
+
+        // Dividing 5 by 3 in case of "float".
+        float myFloatDivide = 5F / 3F;      // 1.6666666
+        System.out.println("Float divide = " + myFloatDivide);
+
+        // Dividing 5 by 3 in case of "double".
+        double myDoubleDivide = 5D / 3D;    // 1.6666666666666667
+        System.out.println("Double divide = " + myDoubleDivide);
+
+        // "double" has a lot more precision than "float".
     }
 }
